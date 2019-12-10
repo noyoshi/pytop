9f69d703d4ef081d83c63ff36895326fd5231efe
Noah Yoshida <nyoshida@nd.edu>
2019-12-10 13:36:04 -0500

    fixed docker issue

--- modules/docker.py
+++ modules/docker.py
@@ -2,9 +2,13 @@ import docker
 from .Classes import DataGroup, OutputData
 
 def docker_usage():
-    containers = docker.from_env().containers.list()
     rows = []
-    for container in containers:
-        rows.append(OutputData(container.label, container.status))
+    try:
+        containers = docker.from_env().containers.list()
+        for container in containers:
+            rows.append(OutputData(container.label, container.status))
+    except:
+        pass
     
     return DataGroup("Docker Info", rows)
+
